36f133d4dac180e082f857c6c50d117a74af95a9
HyJan <hejian@52tt.com>
2021-01-05 19:33:37 +0800

    较大分组的位置

---
+++ algorithm/src/com/leetcode/ArrayByGroupLowerCase.java
@@ -0,0 +1,95 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @program: algorithm
+ * @description: 在一个由小写字母构成的字符串 s 中，包含由一些连续的相同字符所构成的分组。
+ *
+ * 例如，在字符串 s = "abbxxxxzyy" 中，就含有 "a", "bb", "xxxx", "z" 和 "yy" 这样的一些分组。
+ *
+ * 分组可以用区间 [start, end] 表示，其中 start 和 end 分别表示该分组的起始和终止位置的下标。上例中的 "xxxx" 分组用区间表示为 [3,6] 。
+ *
+ * 我们称所有包含大于或等于三个连续字符的分组为 较大分组 。
+ *
+ * 找到每一个 较大分组 的区间，按起始位置下标递增顺序排序后，返回结果。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "abbxxxxzzy"
+ * 输出：[[3,6]]
+ * 解释："xxxx" 是一个起始于 3 且终止于 6 的较大分组。
+ * 示例 2：
+ *
+ * 输入：s = "abc"
+ * 输出：[]
+ * 解释："a","b" 和 "c" 均不是符合要求的较大分组。
+ * 示例 3：
+ *
+ * 输入：s = "abcdddeeeeaabbbcd"
+ * 输出：[[3,5],[6,9],[12,14]]
+ * 解释：较大分组为 "ddd", "eeee" 和 "bbb"
+ * 示例 4：
+ *
+ * 输入：s = "aba"
+ * 输出：[]
+ *  
+ * 提示：
+ *
+ * 1 <= s.length <= 1000
+ * s 仅含小写英文字母
+ * @author: HyJan
+ * @create: 2021-01-05 18:03
+ **/
+public class ArrayByGroupLowerCase {
+
+    /**
+     * 首先要注意的几个点，首先就是要有序，所以外层肯定是一个有序数组
+     * 最重要的是注意边界
+     * @param s
+     * @return
+     */
+    public static List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> lists = new LinkedList<>();
+        if (s == null || "".equals(s.trim())){
+            return lists;
+        }
+        // 定义几个初始化的值
+        int startIndex = 0;
+        char indexStr = s.charAt(0);
+        for (int i = 1;i < s.length(); i++){
+            char c = s.charAt(i);
+            //
+            if (c != indexStr || i == s.length() - 1){
+                // 如果是一直相同的情况，一直走到了字符串的尾部，这时的判断有点异于出现不同字符的情况，又要考虑刚好又是最末尾，但是第三个又是不相同的情况
+                if (i == s.length() - 1 && c == indexStr){
+                    if ((i - startIndex + 1) >= 3){
+                        List<Integer> list = new ArrayList<>();
+                        list.add(startIndex);
+                        list.add(i);
+                        lists.add(list);
+                    }
+                }else if ((i - startIndex) >= 3){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(startIndex);
+                    list.add(i - 1);
+                    lists.add(list);
+                }
+                // 重置初始化数据
+                startIndex = i;
+                indexStr = s.charAt(i);
+            }
+        }
+        return lists;
+    }
+
+
+
+    public static void main(String[] args) {
+        List<List<Integer>> lists = largeGroupPositions("abbxxxxzzy");
+        System.out.println(lists);
+    }
+}
